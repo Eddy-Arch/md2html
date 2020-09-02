6ef4ffb350e9a6e4d68c3d49aa2840fecc27619e
Eddy-Arch <bishclapper69420@gmail.com>
2020-09-02 08:06:39 +0000

    added readme, improved stuff

---
+++ README.md
@@ -0,0 +1,6 @@
+# MD2HTML
+## MD2HTML is a simple markdown to html coverter with some improved syntax
+### its aim is to be minimalistic, simple, and convienient
+#### right now the project is in its infancy, so it only supports the following
+- header support up to h3
+- closing header tags
--- md2html.py
+++ md2html.py
@@ -4,14 +4,17 @@ import io
 
 
 def process(file):
-    bruh = file 
-    processing= bruh.replace("# ", "<h1>").replace("#<h1>", "<h2>")
+    bruh = file
+    processing = bruh.replace("# ", "<h1>").replace(
+        "#<h1>", "<h2>").replace("/#", "</h1>")
     return processing
 
-mdfile = open("index.md","r")
+
+mdfile = open("index.md", "r")
 process_string = mdfile.read()
 processed_string = str(process(process_string))
 
 print(processed_string)
 
-
+with open("index.html", 'a') as out:
+    out.write("<DOCTYPE html>" + '\n' + processed_string)
